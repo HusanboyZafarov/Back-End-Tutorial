896c12f255067eccc876c8b6b67dfe24c1cea41e
HusanboyZafarov <115022906+HusanboyZafarov@users.noreply.github.com>
2022-11-11 13:45:59 +0500

    Back-End

---
+++ travel/blog/admin.py
@@ -0,0 +1,41 @@
+from django.contrib import admin
+from .models import Destination, Hotel, Blog, Comment, Tag, Category
+# Register your models here.
+
+
+@admin.register(Tag)
+class AdminTag(admin.ModelAdmin):
+    list_display = ['id', 'name']
+    list_display_links = ['id', 'name']
+    prepopulated_fields = {'slug': ('name', )}
+
+
+@admin.register(Category)
+class AdminTag(admin.ModelAdmin):
+    list_display = ['id', 'name']
+    list_display_links = ['id', 'name']
+    prepopulated_fields = {'slug': ('name', )}
+
+
+@admin.register(Destination)
+class AdminDestination(admin.ModelAdmin):
+    list_display = ['title', 'price', 'day', 'id']
+    list_display_links = ['title', 'price', 'day', 'id']
+    prepopulated_fields = {'slug': ('title', )}
+
+
+@admin.register(Hotel)
+class AdminHotel(admin.ModelAdmin):
+    list_display = ['title', 'price', 'day', 'id']
+    list_display_links = ['title', 'price', 'day', 'id']
+    prepopulated_fields = {'slug': ('title', )}
+
+
+@admin.register(Blog)
+class AdminBlog(admin.ModelAdmin):
+    list_display = ['title', 'category', 'data_time', 'id']
+    list_display_links = ['title', 'category', 'data_time', 'id']
+    prepopulated_fields = {'slug': ('title', )}
+
+
+admin.site.register(Comment)
---
+++ travel/blog/views.py
@@ -0,0 +1,79 @@
+from django.shortcuts import render, redirect
+from .models import Tag, Destination, Hotel, Blog, Comment, Category
+from .forms import CommentForm
+# Create your views here.
+
+
+def home(request):
+    destinations = Destination.objects.all().order_by('-id')[:4]
+    hotels = Hotel.objects.all().order_by('-id')[:3]
+    context = {
+        'destinations': destinations,
+        'hotels': hotels,
+    }
+    return render(request, 'index.html', context)
+
+
+def about(request):
+    context = {
+
+    }
+    return render(request, 'about.html', context)
+
+
+def destination(request):
+    destinations = Destination.objects.all().order_by('-id')
+    context = {
+        'destinations': destinations
+    }
+    return render(request, 'destination.html', context)
+
+
+def hotels(request):
+    hotels = Hotel.objects.all().order_by('-id')
+    context = {
+        'hotels': hotels
+    }
+    return render(request, 'hotels.html', context)
+
+
+def blog(request):
+    blogs = Blog.objects.all().order_by('-id')
+    context = {
+        'blogs': blogs
+    }
+    return render(request, 'blog.html', context)
+
+
+def blog_detail(request, slug):
+    blog = Blog.objects.get(slug=slug)
+    blogs = Blog.objects.all().order_by('-id')[:3]
+    tags = Tag.objects.all().order_by('id')
+    categories = Category.objects.all()
+    comments = Comment.objects.filter(blog=blog).order_by('-id')
+    if request.method == 'POST':
+        print(request.POST)
+        form = CommentForm(request.POST)
+        if form.is_valid():
+            instance = form.save(commit=False)
+            instance.blog = blog
+            instance.save() 
+            return redirect(f'/blog_detail/{slug}')
+    else:
+        form = CommentForm()
+    context = {
+        'blogs': blogs,
+        'blog': blog,
+        'tags': tags,
+        'categories': categories,
+        'comments': comments,
+        'form': form
+    }
+    return render(request, 'blog-single.html', context)
+
+
+def contact(request):
+    context = {
+
+    }
+    return render(request, 'contact.html', context)
