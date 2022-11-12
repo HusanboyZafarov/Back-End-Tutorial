ddf470a30b6504ea6957d2e7367bcbcffa23ff84
HusanboyZafarov <zhzr1222@gmail.com>
2022-11-12 06:29:25 +0500

    Commiting to main

--- travel/blog/views.py
+++ travel/blog/views.py
@@ -9,46 +9,56 @@ def home(request):
     destinations = Destination.objects.all().order_by('-id')[:4]
     hotels = Hotel.objects.all().order_by('-id')[:3]
     blogs = Blog.objects.all().order_by('-id')[:3]
+    categories = Category.objects.all().order_by('-id')
     context = {
         'destinations': destinations,
         'hotels': hotels,
-        'blogs': blogs
+        'blogs': blogs,
+        'categories': categories,
     }
     return render(request, 'index.html', context)
 
 
 def about(request):
+    categories = Category.objects.all().order_by('-id')
     context = {
-
+        'categories': categories,
     }
     return render(request, 'about.html', context)
 
 
 def destination(request):
+    categories = Category.objects.all().order_by('-id')
     destinations = Destination.objects.all().order_by('-id')
     context = {
+        'categories': categories,
         'destinations': destinations
     }
     return render(request, 'destination.html', context)
 
 
 def hotels(request):
+    categories = Category.objects.all().order_by('-id')
     hotels = Hotel.objects.all().order_by('-id')
     context = {
+        'categories': categories,
         'hotels': hotels
     }
     return render(request, 'hotels.html', context)
 
 
 def blog(request):
+    categories = Category.objects.all().order_by('-id')
     blogs = Blog.objects.all().order_by('-id')
     context = {
+        'categories': categories,
         'blogs': blogs
     }
     return render(request, 'blog.html', context)
 
 
 def blog_detail(request, slug):
+    categories = Category.objects.all().order_by('-id')
     blog = Blog.objects.get(slug=slug)
     blogs = Blog.objects.all().order_by('-id')[:3]
     tags = Tag.objects.all().order_by('id')
@@ -70,6 +80,7 @@ def blog_detail(request, slug):
         'tags': tags,
         'categories': categories,
         'comments': comments,
+        'categories': categories,
         'form': form
     }
     return render(request, 'blog-single.html', context)
@@ -86,17 +97,20 @@ def category(request):
 
 
 def category_detail(request, slug):
+    categories = Category.objects.all().order_by('-id')
     category = Category.objects.get(slug=slug)
     blogs = Blog.objects.filter(category=category)
     context = {
         'blogs': blogs,
         'object': category,
+        'categories': categories,
     }
     return render(request, 'category_detail.html', context)
 
 
 def contact(request):
+    categories = Category.objects.all().order_by('-id')
     context = {
-
+        'categories': categories,
     }
     return render(request, 'contact.html', context)
